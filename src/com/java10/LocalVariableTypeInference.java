202f3bba42b6a2fd6c5f9c9db5e499156769c6fb
Naresh <naresh.chinthakindi@gmail.com>
2020-04-25 14:30:05 +0530

    New implementation of java 10 classes

---
+++ src/com/java10/LocalVariableTypeInference.java
@@ -0,0 +1,27 @@
+package com.java10;
+
+import java.util.HashMap;
+
+public class LocalVariableTypeInference {
+
+    //var val = "nares"; cannot initialize here
+
+    //var val = "nares"; cannot resolve value
+
+    public static void main(String[] args) {
+        var str = "Naresh";
+
+
+        var age = 33;
+
+        System.out.println("Name : "+str+" age "+age);
+
+        var val = new HashMap<String, String>();
+
+        val.put("naresh", "test");
+        System.out.println(val.get("naresh"));
+
+        //var x = null;//Cannot inferer
+    }
+
+}
